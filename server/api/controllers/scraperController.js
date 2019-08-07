c8f280b3f460bbc121acce290a2ac43e9f86fa52
benAkehurst <benakehurst@hotmail.com>
2019-08-07 22:28:33 +0100

    feat(scraper): add basic puppteer script to get title and price

--- server/api/controllers/scraperController.js
+++ server/api/controllers/scraperController.js
@@ -1,7 +1,38 @@
 'use strict';
 const mongoose = require('mongoose');
+const Item = mongoose.model('Item');
+
+const puppeteer = require('puppeteer');
 
 exports.scrapePage = (req, res) => {
-  console.log(req.body);
-  res.send({msg:'in scrape page function'});
+  let url = req.body.url;
+  scraper(url).then(value => {
+    console.log(value);
+  });
 };
+
+let scrape = async () => {
+  return 'test';
+};
+
+let scraper = async (url) => {
+  console.log('Accessing Amazon to fetch data');
+  
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  await page.goto(url);
+  await page.waitFor(1000);
+
+  const result = await page.evaluate(() => {
+    let title = document.querySelector('#productTitle').innerText;
+    let priceStr = document.querySelector('#priceblock_ourprice').innerText;
+    let priceInt = parseInt(priceStr.replace(/£/g, ''));
+    return {
+      title,
+      priceInt
+    };
+  });
+  browser.close();
+  return result;
+}
