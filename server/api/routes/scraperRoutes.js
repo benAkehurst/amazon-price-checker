894d5cc22ea1445a31cb8df174bb6d72773f8144
benAkehurst <benakehurst@hotmail.com>
2019-08-10 12:27:43 +0100

    update(scrape routes): update routes

--- server/api/routes/scraperRoutes.js
+++ server/api/routes/scraperRoutes.js
@@ -5,9 +5,9 @@ module.exports = app => {
   /**
    * Scraper Routes - Item Collection
    */
-  app.route('/api/scrapeData').get(scraperController.get_all_items);
-  app.route('/api/scrapeData/:itemId').get(scraperController.get_single_item);
-  app.route('/api/initalScrapeOfItem').post(scraperController.first_scrape);
+  app.route('/api/get-all-product-data').get(scraperController.get_all_items);
+  app.route('/api/scrape-single-item').post(scraperController.get_single_item);
+  app.route('/api/initial-add-product').post(scraperController.first_scrape);
   app.route('/api/updateScrapeItem').post(scraperController.update_item);
   app.route('/api/removeScrapeItem/:itemId').delete(scraperController.delete_item);
 };
