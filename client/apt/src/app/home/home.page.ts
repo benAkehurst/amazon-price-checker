afd4812bfbc041263d23792f18b97ce87d6d612b
benAkehurst <benakehurst@hotmail.com>
2019-08-18 19:13:41 +0100

    feature(home): add list of items to home

--- client/apt/src/app/home/home.page.ts
+++ client/apt/src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { DataService } from './../services/data.service';
 import { IItem } from '../interfaces/item.interface';
 
@@ -12,17 +14,22 @@ export class HomePage implements OnInit {
   public errorMessage: string = '';
   public isLoading: boolean = false;
 
-  constructor(private dataService: DataService) {}
+  constructor(private dataService: DataService, private router: Router) {}
 
   ngOnInit() {
-   this.getAllItems();
+    this.isLoading = true;
+    this.getAllItems();
+  }
+
+  ionViewWillEnter() {
+    this.isLoading = true;
+    this.getAllItems();
   }
 
   /**
    * Fetchs all the items in the Database
    */
   public getAllItems() {
-    this.isLoading = true;
     this.dataService.getAllItems().subscribe(res => {
       if (res.success === false) {
         this.errorMessage = res.message;
@@ -30,9 +37,15 @@ export class HomePage implements OnInit {
       this.loadedItems = res.data;
       if (this.loadedItems.length > 0) {
         this.dataService.addItemsToLocalStorage(this.loadedItems);
+        this.isLoading = false;
       }
-      this.isLoading = false;
-      console.log(this.loadedItems);
     });
+    console.log(this.loadedItems);
+  }
+
+  public goToItemPage(item: IItem) {
+    this.dataService.selectedItem = item;
+    this.dataService.selectedPage = item._id;
+    this.router.navigateByUrl(`/single-item/${item._id}`);
   }
 }
