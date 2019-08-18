d6d1dfd92fa7a6596abba62223bbc2e447789d6a
benAkehurst <benakehurst@hotmail.com>
2019-08-18 18:07:35 +0100

    feat(interfaces): add items interfaces

---
+++ client/apt/src/app/interfaces/item.interface.ts
@@ -0,0 +1,18 @@
+export class IItem {
+  public id: string;
+  public name: string;
+  public link: string;
+  public imgUrl: string;
+  public price: number;
+  public targetPrice: number;
+  public following: boolean;
+  public pastPrices: Array<object>;
+  public createdDate: Date;
+  public updatedDate: Date;
+}
+
+export class INewItem {
+  public url: string;
+  public targetPrice: number;
+  public following: boolean;
+}
