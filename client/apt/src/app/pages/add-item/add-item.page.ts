181e90b3032e652b96f55537b1669573a803bb22
benAkehurst <benakehurst@hotmail.com>
2019-08-18 22:58:22 +0100

    feat(add-item): add item working url check

--- client/apt/src/app/pages/add-item/add-item.page.ts
+++ client/apt/src/app/pages/add-item/add-item.page.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+
+import { DataService } from '../../services/data.service';
+import { INewItem } from '../../interfaces/item.interface';
 
 @Component({
   selector: 'app-add-item',
@@ -7,13 +10,36 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./add-item.page.scss']
 })
 export class AddItemPage implements OnInit {
+  @ViewChild('f', {static: true}) form: NgForm;
   public isLoading = false;
+  public newItem: INewItem;
 
-  constructor() {}
+  constructor(private dataService: DataService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.isLoading = true;
+  }
 
   public onAddItem() {
+    const itemData = {
+      amazonUrl: this.validateUrlStr(this.form.value['amazon-url']),
+      trackPrice: this.form.value['track-price'],
+      targetPrice: this.form.value['target-price']
+    };
+    console.log(itemData);
+  }
 
+  public validateUrlStr(targetUrl: string) {
+    const url = targetUrl;
+    const pattern = new RegExp(
+      '^(https?:\\/\\/)?' + // protocol
+      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+        '(\\#[-a-z\\d_]*)?$',
+      'i'
+    ); // fragment locator
+    return !!pattern.test(url);
   }
 }
