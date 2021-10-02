c37478bb7b588049eee3f472911da4c6b80779aa
Ben Akehurst <15856820+benAkehurst@users.noreply.github.com>
2021-10-02 10:47:46 +0100

    update email service to remove switch case

--- server/middlewares/services/emailService.js
+++ server/middlewares/services/emailService.js
@@ -8,55 +8,23 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-/**
- * Sends email to the user when the price is hit
- * @param {*} data {
- *  toEmail: The person to send the email to
- *  itemName: The item name
- *  itemLink: The item URL
- * }
- */
 const sendEmail = (data) => {
-  let mailOptions = {
-    from: '',
-    to: '',
-    subject: '',
-    html: '',
-  };
-  switch (data.source) {
-    case 'signup':
-      mailOptions = {
-        from: data.from,
-        to: data.to,
-        subject: data.subject,
-        html: data.html,
-      };
-      break;
-    case 'pricehit':
-      mailOptions = {
-        from: `${PROCESS.env.EMAIL_USERNAME}`,
-        to: `${data.toEmail}`,
-        subject: `AMAZON PRICE TRACK - ${data.itemName} HAS HIT YOUR TARGET PRICE`,
-        html: `<p>The ${data.itemName} you wanted to buy is now at your target price! <a href="${data.itemLink}">Buy it HERE!</a></p>`,
-      };
-      break;
-    case 'resetPassword':
-      mailOptions = {
-        from: data.from,
-        to: data.to,
-        subject: data.subject,
-        html: data.html,
-      };
-      break;
-  }
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent successfully');
-      return true;
+  transporter.sendMail(
+    {
+      from: data.from,
+      to: data.to,
+      subject: data.subject,
+      html: data.html,
+    },
+    (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent successfully');
+        return true;
+      }
     }
-  });
+  );
 };
 
 module.exports = { sendEmail };
